e71986b5cfdc9c4c0980f40244ab4ead0794cbdd
neil3k <78107504+neil3k@users.noreply.github.com>
2023-09-19 17:03:28 +0100

    remove spurious comments

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,8 +1,6 @@
 resource "aws_iam_role" "lambda_minecraft_role" {
   name = "lambda_minecraft_role"
 
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -25,9 +23,7 @@ resource "aws_iam_role" "lambda_minecraft_role" {
 resource "aws_iam_role_policy" "lambda_minecraft_policy" {
   name = "lambda_minecraft_policy"
   role = aws_iam_role.lambda_minecraft_role.id
-
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
+  
   policy = jsonencode(
     {
       "Version" : "2012-10-17",
